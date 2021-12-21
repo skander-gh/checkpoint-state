4733aa0762f7798cea2cf5fa1c9fe302315400de
skander-gh <94468839+skander-gh@users.noreply.github.com>
2021-12-21 09:34:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Person from './Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state={x:false}
+
+  toggleshow=()=>{  this.setState({x:!this.state.x}) }
+
+render(){
+  return(
+<div>
+  <button onClick={this.toggleshow}>show</button>
+  {this.state.x==true? <Person></Person> :null }
+  
+</div>
+  )
 }
 
+}
 export default App;
---
+++ src/Person.js
@@ -0,0 +1,18 @@
+import React from 'react' ;
+class Person extends React.Component{
+
+   
+
+    render(){
+        return(
+        <div>
+           <h3>Skander</h3>
+           <h3>21</h3>
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyQ8zYhDk0Pc93JtIMk6TxGLv6Klo1Aw3ufw&usqp=CAU"/>
+           <h3> GOMYCODE Student </h3>
+        </div>
+        )
+    }
+}
+
+export default Person
